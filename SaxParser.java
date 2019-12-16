21f6a77611a24680a3ffffc4ede642e49e1c61b0
Wiebren <w.van.der.vaart@st.hanze.nl>
2019-12-16 10:31:43 +0100

    project files
    push project files

---
+++ SaxParser.java
@@ -0,0 +1,83 @@
+package shoutingMTserver;
+
+import java.io.StringReader;
+import java.sql.*;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+class Parser implements Runnable {
+
+	@Override
+	public void run() {
+		try{  
+			Class.forName("com.mysql.jdbc.Driver");  
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/unwdmi","root","");  
+			System.out.println(con);
+			DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputSource src = new InputSource();
+			Document doc = null;
+			Statement stmt = con.createStatement();  
+			String stn = ""; 
+			String date = ""; 
+			String time = "";
+			String temp = ""; 
+			String dewp = ""; 
+			String stp = "";
+			String slp = "";
+			String visib = "";
+			String wdsp = "";
+			String prcp = "";
+			String sndp = "";
+			String frshtt = "";
+			String cldc = "";
+			String wnddir = "";
+			while(true) 
+			{
+				if(ShoutingMTServer.xmlData.size() > 0) {
+					//System.out.println(xmlData.size());
+					String data = ShoutingMTServer.xmlData.get(0);
+					data = data.replaceAll("\\s+","");
+					ShoutingMTServer.xmlData.remove(0);
+					src.setCharacterStream(new StringReader(data));
+					doc = builder.parse(src);
+					stn = doc.getElementsByTagName("STN").item(0).getTextContent();
+					date = doc.getElementsByTagName("DATE").item(0).getTextContent();
+					time = doc.getElementsByTagName("TIME").item(0).getTextContent();
+					temp = doc.getElementsByTagName("TEMP").item(0).getTextContent();
+					dewp = doc.getElementsByTagName("DEWP").item(0).getTextContent();
+					stp = doc.getElementsByTagName("STP").item(0).getTextContent();
+					slp = doc.getElementsByTagName("SLP").item(0).getTextContent();
+					visib = doc.getElementsByTagName("VISIB").item(0).getTextContent();
+					wdsp = doc.getElementsByTagName("WDSP").item(0).getTextContent();
+					prcp = doc.getElementsByTagName("PRCP").item(0).getTextContent();
+					sndp = doc.getElementsByTagName("SNDP").item(0).getTextContent();
+					frshtt = doc.getElementsByTagName("FRSHTT").item(0).getTextContent();
+					cldc = doc.getElementsByTagName("CLDC").item(0).getTextContent();
+					wnddir = doc.getElementsByTagName("WNDDIR").item(0).getTextContent();
+					stmt=con.createStatement(); 
+					
+					System.out.println("INSERT INTO unwdmi.measurements VALUES (" + stn + "," + date + ",'" + time + "'," +temp+ "," +dewp+ "," +stp+ "," +slp+ "," +visib+ "," +wdsp+ "," +prcp+ "," +sndp+ "," +frshtt+ "," +cldc+ "," +wnddir +")");
+					
+					if(stn==""||date==""||time==""||temp==""||dewp==""||stp==""||slp==""||visib==""||wdsp==""||prcp==""||sndp==""||frshtt==""||cldc==""||wnddir=="") {
+						System.out.println("Data incomplete");
+					}else {
+						System.out.println("New insert");
+						stmt.executeUpdate("INSERT INTO unwdmi.measurements VALUES (" + stn + ",'" + date + "','" + time + "'," +temp+ "," +dewp+ "," +stp+ "," +slp+ "," +visib+ "," +wdsp+ "," +prcp+ "," +sndp+ "," +frshtt+ "," +cldc+ "," +wnddir +")");
+						System.out.println("insert done. inserts left to do: " + ShoutingMTServer.xmlData.size());
+					}
+				}else 
+				{
+					System.out.println("No data found to insert");
+				}
+			}
+			
+			//con.close();  
+			}catch(Exception e){ 
+				System.out.println(e + " NULL");
+			}
+	}  
+}  
---
+++ ShoutingThread.java
@@ -0,0 +1,55 @@
+package shoutingMTserver;
+
+import java.net.*;
+import java.io.*;
+//import java.util.concurrent.*;
+
+class Worker implements Runnable
+{
+	private Socket connection;
+
+	public Worker(Socket connection) {
+		this.connection = connection;
+	}
+
+	public void run() {
+		try {
+			String s;
+			//System.err.println("New worker thread started");
+			
+			//lets check if we already accepted maximum number of connections
+			ShoutingMTServer.mijnSemafoor.probeer();
+			
+			PrintWriter pout = new PrintWriter(connection.getOutputStream(), true);
+			BufferedReader bin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			String currentdata = "";
+			while ((s = bin.readLine()) != null) {
+				if(s.equals("	<MEASUREMENT>")) {
+					currentdata = "";
+					currentdata = currentdata + s;
+					
+				}
+				if(s.equals("	</MEASUREMENT>")) {
+					currentdata = currentdata + s;
+					ShoutingMTServer.xmlData.add(currentdata);
+				}
+						
+				if(s.equals("	</MEASUREMENT>")|| s.equals("	<MEASUREMENT>")){
+					//do nothing here
+				}else{
+					currentdata = currentdata + s;
+				}         
+	        }
+	
+			// now close the socket connection
+			connection.close();
+			System.err.println("Connection closed: workerthread ending");
+			// upping the semaphore.. since the connnection is gone....
+			ShoutingMTServer.mijnSemafoor.verhoog();
+		}
+		catch (IOException ioe) { }
+		catch (InterruptedException ie) {}
+	}
+}
+
